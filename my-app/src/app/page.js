59449e56cf28b82b10e34d2fc0b60ec24981d525
abbazsr795 <abbazsr795@gmail.com>
2024-06-12 23:01:38 +1000

    Added most info

---
+++ my-app/src/app/page.js
@@ -0,0 +1,78 @@
+import Image from "next/image";
+import Github from "./icons/github-1.png"
+import Linkedin from "./icons/linkedin-1.png"
+import Instagram from "./icons/instagram-1.png"
+import X from "./icons/x-1.png"
+
+export default function Home() {
+  return (
+    <div className="flex flex-row h-screen text-wrap">
+      <div className="bg-white p-10 w-1/2 h-full">
+        <p className="font-mono font-semibold text-7xl pb-10">Abbazs Rahman</p>
+        <p className="font-mono font-normal text-2xl pb-10">Computer Science Major @RMIT</p>
+        <p className="font-mono font-normal text-2xl pb-10 text-sky-700">Tech enthusiast exploring the cutting-edge realms of cloud computing, back-end development, and artificial intelligence.</p>
+        <div className="flex flex-row gap-10">
+          <a href="https://github.com/abbazsr795"><Image src={Github} width={40}></Image></a>
+          <a href="https://www.linkedin.com/in/abbazs-rahman-968b65260/"><Image src={Linkedin} width={40}></Image></a>
+          <a href="https://www.instagram.com/abbazs.r/"><Image src={Instagram} width={40}></Image></a>
+          <a href="https://x.com/abbazsr795"><Image src={X} width={40}></Image></a>
+        </div>
+      </div>
+      <div className="bg-white p-10 w-1/2 h-full overflow-auto">
+        <div>
+          <p className="font-mono font-bold text-5xl pb-5">Education</p>
+          <p className="font-mono font-bold text-2xl pb-2">Royal Melbourne Institute of Technology, Australia</p>
+          <p className="font-mono font-normal text-2xl pb-5">Bachelor of Computer Science</p>
+          <p className="font-mono font-bold text-2xl pb-2">Gateway College, Sri Lanka</p>
+          <p className="font-mono font-normal text-2xl pb-20 text-balance">Edexcel/Cambridge International A Level in Mathematics (592/600 - A* - Highest Mark), Further Mathematics (546/600 - A* - Highest Mark) and Physics (569/600 -  A* - Highest Mark), Computer Science (87/100 - A)</p>
+        </div>
+        <div>
+          <p className="font-mono font-bold text-5xl pb-5">License and Certification</p>
+          <p className="font-mono font-bold text-2xl pb-2">Machine Learning & Data Science Bootcamp</p>
+          <p className="font-mono font-normal text-2xl pb-5">By Andrei Neagoie and Daniel Bourke</p>
+          <p className="font-mono font-bold text-2xl pb-2">Full Stack Web Development Bootcamp</p>
+          <p className="font-mono font-normal text-2xl pb-5">By Andrei Neagoie</p>
+          <p className="font-mono font-bold text-2xl pb-2">AWS Certified Cloud Practitioner</p>
+          <p className="font-mono font-normal text-2xl pb-5">By Stéphane Maarek</p>
+          <p className="font-mono font-bold text-2xl pb-2">International Computer Driving License</p>
+          <p className="font-mono font-normal text-2xl pb-20">In Digital Marketing, Advanced Spreadsheets, Word Processing and Presentation by ICDL Asia</p>
+        </div>
+        <div>
+          <p className="font-mono font-bold text-5xl pb-5">Skills and Technologies</p>
+          <div className="flex flex-row flex-wrap gap-5 pb-20">
+            <p className="font-mono font-normal text-xl p-3 bg-sky-200 rounded-md">Python</p>
+            <p className="font-mono font-normal text-xl p-3 bg-sky-200 rounded-md">Javascript</p>
+            <p className="font-mono font-normal text-xl p-3 bg-sky-200 rounded-md">HTML</p>
+            <p className="font-mono font-normal text-xl p-3 bg-sky-200 rounded-md">CSS</p>
+            <p className="font-mono font-normal text-xl p-3 bg-sky-200 rounded-md">Java</p>
+            <p className="font-mono font-normal text-xl p-3 bg-sky-200 rounded-md">Python</p>
+            <p className="font-mono font-normal text-xl p-3 bg-sky-200 rounded-md">ReactJS</p>
+            <p className="font-mono font-normal text-xl p-3 bg-sky-200 rounded-md">NextJS</p>
+            <p className="font-mono font-normal text-xl p-3 bg-sky-200 rounded-md">Git</p>
+            <p className="font-mono font-normal text-xl p-3 bg-sky-200 rounded-md">Firebase</p>
+          </div>
+        </div>
+        <div>
+          <p className="font-mono font-bold text-5xl pb-5">Projects</p>
+          <p className="font-mono font-bold text-2xl pb-2">CNN Image Classification Model</p>
+          <p className="font-mono font-normal text-2xl pb-5 text-balance">The Jupyter notebook showcases the development of a Convolutional Neural Network (CNN) Image Classification model. This model is designed to distinguish between two specific categories of images, such as happy and sad individuals. Additionally, the workflow includes a data pipeline aimed at enhancing efficiency and facilitating the integration of other image categories into the process. The tools used for this project are Python, Numpy, Pandas, Scikit-Learn and Tensorflow.</p>
+          <p className="font-mono font-bold text-2xl pb-2">RacketIdeas</p>
+          <p className="font-mono font-normal text-2xl pb-5 text-balance">A startup I'm currently working on that facilitates connections between business owners and potential investors and partners. Additionally, the platform allows businesses to gather feedback from the public through voting to assess their success rate and receive constructive criticisms through comments. The website was built using Next.js, Firebase, TailwindCSS, EvergreenUI, and Zustand for state management.</p>
+          <p className="font-mono font-bold text-2xl pb-2">Racket Design</p>
+          <p className="font-mono font-normal text-2xl pb-20 text-balance">A versatile collection of web components crafted with a unique design ethos. Inspired by the innovative creations of Racket Labs, the startup I'm proud to be a part of, these components embody elegance and functionality.</p>
+        </div>
+        <div>
+          <p className="font-mono font-bold text-5xl pb-5">Honors and Awards</p>
+          <p className="font-mono font-bold text-2xl pb-2">High Achiever Award for Edexcel International A Level</p>
+          <p className="font-mono font-normal text-2xl pb-5">For achieving 3 A*s</p>
+          <p className="font-mono font-bold text-2xl pb-2">Young Computer Scientist (YCS) 2019</p>
+          <p className="font-mono font-normal text-2xl pb-5">Issued by Federation of IT Industry Sri Lanka (FITIS)</p>
+          <p className="font-mono font-bold text-2xl pb-2">KRUU Project - Merit and Eminence Class</p>
+          <p className="font-mono font-normal text-2xl pb-5">By KRUU Project</p>
+          <p className="font-mono font-bold text-2xl pb-2">SPARK Youth Entrepreneur Competition Top 105</p>
+          <p className="font-mono font-normal text-2xl pb-20">By Sri lankan Chamber of Commerce</p>
+        </div>
+      </div>
+    </div>
+  );
+}
